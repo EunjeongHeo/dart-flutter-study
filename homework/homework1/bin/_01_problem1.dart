83885ebe500d7278a43092c73743691d93eb7697
Joy <munchinthepool64@gmail.com>
2025-12-05 11:35:51 +0900

    feat: 필수문제 1번 작성 완료

--- homework/homework1/bin/_01_problem1.dart
+++ homework/homework1/bin/_01_problem1.dart
@@ -1,4 +1,14 @@
-void main() {
-  print("문제1 풀이");
-  // 여기에 문제 1 풀이 코드 작성
+void main(){
+  print('필수 문제 1번');
+	int score = 84;
+	String grade = '';
+	
+	if(score >= 90 && score < 100){
+		grade = 'A';
+	} else if(score >= 80 && score < 90){
+		grade = 'B';
+	} else {
+		grade = 'C';
+	} 
+	print('이 학생의 점수는 $score점 이며, 등급은 $grade등급입니다!');
 }
